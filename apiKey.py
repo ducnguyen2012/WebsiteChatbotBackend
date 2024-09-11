184a73297354e56e9140ee60f7aa1dd15772d5c2
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2024-09-11 02:01:34 +0100

    Add files via upload

--- apiKey.py
+++ apiKey.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv, dotenv_values
+import os
+load_dotenv()
+
 def api():
     '''
     return api key if you adjust this!
     '''
-    return "API KEY IN HERE"
+    return os.getenv("MY_SECRET_KEY")
