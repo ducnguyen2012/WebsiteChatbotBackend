aa2738fd8d9d89d101c5efa80de4a65f5710df60
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2024-09-11 03:55:26 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -35,4 +35,4 @@ def uploadQuestion():
     return jsonify(question)
     
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
